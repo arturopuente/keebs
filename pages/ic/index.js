4f31b8efd17b1442eb4f3adf8b3e0e97d8491a11
Arturo Puente <arturopuentevc@gmail.com>
2020-07-13 03:15:11 -0500

    Parse interest check titles and authors

--- pages/ic/index.js
+++ pages/ic/index.js
@@ -1,7 +1,43 @@
+import useSWR from "swr";
+import cheerio from "cheerio";
+
+const fetcher = (...args) => fetch(...args).then(res => res.text());
+
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
+const BASE_URL = "https://geekhack.org/index.php?board=132.0";
+
+const extract = data => {
+  if (!data) return [];
+  const items = [];
+  const query = cheerio.load(data);
+  const listingQuery = "#messageindex tbody tr td.subject:not(.locked_sticky2)";
+  const listings = query(listingQuery);
+  for (var i = 0; i < listings.length; i++) {
+    const qx = cheerio.load(listings[i]);
+    items.push({
+      title: qx("span a").text(),
+      author: qx("p > a:first-child").text()
+    });
+  }
+  return items;
+};
+
 export default function InterestChecks() {
+  const { data, error } = useSWR(PROXY_URL + BASE_URL, fetcher, {
+    revalidateOnFocus: false
+  });
+  const listings = extract(data);
+
   return (
     <div>
       <h1>Interest Checks</h1>
+      <ul>
+        {listings.map(listing => (
+          <li key={listing.title}>
+            {listing.title} [{listing.author}]
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
