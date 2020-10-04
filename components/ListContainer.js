1924c066a8143a34c57ddcb97d712a794d40c152
Arturo Puente <arturopuentevc@gmail.com>
2020-10-04 02:50:49 -0500

    Account for scroll position on mobile devices

--- components/ListContainer.js
+++ components/ListContainer.js
@@ -2,7 +2,7 @@ import { Header, NavBar } from "./index";
 
 export default function ListContainer({ children }) {
   return (
-    <div className={"max-w-screen-lg mx-auto px-5"}>
+    <div className={"max-w-screen-lg mx-auto px-5 pb-10"}>
       <Header />
       <NavBar />
       {children}
