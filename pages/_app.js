492f866d06c786c880b9e80c9413a1ddd70d00ab
Arturo Puente <arturopuentevc@gmail.com>
2020-10-04 03:16:34 -0500

    Set page body background to avoid color transitions

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "../styles/tailwind.css";
 
 export default function Keebs({ Component, pageProps }) {
   return (
-    <div className={"w-full h-full bg-gray-900 text-white font-sans"}>
+    <div className={"w-full h-full text-white font-sans"}>
       <Head>
         <title>Keyboards by @arturopuente</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
